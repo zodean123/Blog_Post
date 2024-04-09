a5981d8775bad384295f5ddcd0bfedcb1d9337b3
zodean123 <dipanmallick7085@gmail.com>
2024-04-09 19:09:09 +0530

    Authenticating user

--- api/index.js
+++ api/index.js
@@ -5,12 +5,15 @@ const User = require("./models/user.js");
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const PORT = 4000;
+const jwt = require("jsonwebtoken");
+const cookieParser = require('cookie-parser');
+
 
 const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfasdgouhfgjhaslfjasdf';
 
 
-
-app.use(cors());
+app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/user')
@@ -28,6 +31,23 @@ app.post('/register',async (req,res)=>{
     }
  
 })
+
+app.post('/login',async (req,res)=>{
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username:username});
+  const passok =  bcrypt.compareSync(password,userDoc.password);
+  if(passok){
+   jwt.sign({username,id:userDoc._id},secret,{},(err,token)=>{
+    if(err) throw err;
+    res.cookie('token',token).json('ok');
+});
+  }
+  else{
+    res.status(400).json('wrong credentials');
+  }
+})
+
+
 
 app.listen(PORT, () => {
     console.log(`Connection is live at port no. ${PORT}`);
